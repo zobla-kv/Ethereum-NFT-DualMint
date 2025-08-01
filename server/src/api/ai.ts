d31352fa1c4e1ab6aeafe3ae571392c8012c54a3
zobla <zobla.kv@gmail.com>
2025-08-01 09:19:00 +0200

    move limiter to endpoint

--- server/src/api/ai.ts
+++ server/src/api/ai.ts
@@ -1,8 +1,21 @@
 import { Router, Request, Response } from 'express';
+import rateLimit from 'express-rate-limit';
 
 const aiRouter = Router();
 
-aiRouter.post('/', (req: Request, res: Response) => {
+// rate limiter
+const limiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000,
+  limit: 10,
+  message: {
+    status: 429,
+    error: 'Too many requests',
+    message:
+      'You have reached the maximum number of images today. Please try again after 24 hours.',
+  },
+});
+
+aiRouter.post('/', limiter, (req: Request, res: Response) => {
   res.status(200).json({ response: 'image' });
 });
 
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,9 @@
 // app
 import http from 'http';
 import express from 'express';
-import rateLimit from 'express-rate-limit';
 
 const app = express();
+const SERVER_PORT = 4600;
 
 // cors
 import cors from './middleware/cors';
@@ -11,22 +11,8 @@ import cors from './middleware/cors';
 // endpoints
 import apiRouter from './api/index';
 
-// rate limiter
-const limiter = rateLimit({
-  windowMs: 24 * 60 * 60 * 1000,
-  limit: 10,
-  message: {
-    status: 429,
-    error: 'Too many requests',
-    message:
-      'You have reached the maximum number of images today. Please try again after 24 hours.',
-  },
-});
-
 // api handlers
-app.use('/api', cors, limiter, apiRouter);
-
-const SERVER_PORT = 4600;
+app.use('/api', cors, apiRouter);
 
 // http server
 http.createServer(app).listen(SERVER_PORT, () => {
