def47476adb629e9326be9eee652bbe1b33ffa51
zobla <zobla.kv@gmail.com>
2025-09-09 22:57:44 +0200

    add script for funding metamask account on localhost network

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -1,6 +1,7 @@
 import type { HardhatUserConfig } from 'hardhat/config';
 
 import hardhatToolboxViemPlugin from '@nomicfoundation/hardhat-toolbox-viem';
+import { configVariable } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxViemPlugin],
@@ -10,6 +11,7 @@ const config: HardhatUserConfig = {
       chainId: 31337,
       type: 'http',
       url: 'http://127.0.0.1:8545/',
+      accounts: [configVariable('LOCAL_PRIVATE_KEY')],
     },
   },
 };
---
+++ apps/contracts/scripts/fundLocalhost.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { createWalletClient, http, parseEther } from 'viem';
+import { waitForTransactionReceipt } from 'viem/actions';
+import { privateKeyToAccount } from 'viem/accounts';
+import { localhost } from 'viem/chains';
+
+const funder = privateKeyToAccount(
+  process.env.LOCAL_PRIVATE_KEY as `0x${string}`
+);
+
+const recipient = '0x4eE078C15c7926C174e32346f4210c9764eF7Cc6';
+
+const client = createWalletClient({
+  account: funder,
+  chain: { ...localhost, id: 31337 },
+  transport: http('http://127.0.0.1:8545'),
+});
+
+const txHash = await client.sendTransaction({
+  to: recipient,
+  value: parseEther('0.1'), // amount in ETH
+});
+
+const receipt = await waitForTransactionReceipt(client, { hash: txHash });
+console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
